fb380166942b352c517c9f6dc4331d82b1fe6c94
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-04-21 13:00:59 -0700

    working on user story 4

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,4 @@
 class Animal < ApplicationRecord
   has_many :sightings
+  validates :common_name, :scientific_binomial, presence: true
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    animal = Animal.create(common_name:'Cat validation', scientific_binomial:'Meowy mix alot')
+    expect(animal).to be_valid
+  end
+
+  it 'is not valid without a common_name' do
+    animal = Animal.create(scientific_binomial:'Meowy mix alot')
+    expect(animal.errors[:common_name]).to_not be_empty
+  end
+
+  it 'is not valid without a scientific_binomial' do
+    animal = Animal.create(common_name:'cat validation')
+    expect(animal.errors[:scientific_binomial]).to_not be_empty
+  end
 end
+
+  
